32633048f28ca912c12e172858d1f919c0988acb
John van Noort <jvannoort@gmail.com>
2022-07-08 17:13:29 +0200

    coords in df

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def create_ref_image(period=10, width=100, size=100):
 
 
 def fit_peak(X, Y, Z, show=False):
-    # Fit a two-dimensional Gaussian
+    # Our function to fit is a two-dimensional Gaussian
     def gaussian(x, y, x0, y0, sigma, A):
         return A * np.exp(-((x - x0) / sigma) ** 2 - ((y - y0) / sigma) ** 2)
 
@@ -113,75 +113,6 @@ def fit_peak(X, Y, Z, show=False):
     return fit, np.asarray(pars)
 
 
-def find_beads(im, roi_size=100, n_max=100, treshold=None, show=False):
-    @nb.njit(parallel=True, fastmath=True, cache=True)
-    def bin2d(a, K):
-        assert a.shape[0] % K == 0
-        assert a.shape[1] % K == 0
-        m_bins = a.shape[0] // K
-        n_bins = a.shape[1] // K
-        res = np.zeros((m_bins, n_bins), dtype=a.dtype)
-        for i in nb.prange(res.shape[0]):
-            for ii in range(i * K, (i + 1) * K):
-                for j in range(res.shape[1]):
-                    TMP = res[i, j]
-                    for jj in range(j * K, (j + 1) * K):
-                        TMP += a[ii, jj]
-                    res[i, j] = TMP
-        return res
-
-    reduction = 2  # scale down image by factor 2
-    reduced_im = bin2d(im, reduction)
-    reduced_im -= np.median(reduced_im)
-
-    cc = np.ones_like(reduced_im)
-    for period in tqdm([6.5, 8.7, 10.3, 12.7], postfix='Cross correlating with ref images'):
-        ref_im = create_ref_image(period, size=len(reduced_im))
-        cc *= np.abs(np.fft.ifft2(np.fft.fft2(reduced_im) * np.conjugate(np.fft.fft2(ref_im))))
-    cc = np.abs(np.fft.fftshift(cc))
-    cc /= np.percentile(cc, 99.999)
-
-    X, Y = np.meshgrid(reduction * np.arange(len(cc)), reduction * np.arange(len(cc)))
-
-    coords_list = []
-    for _ in tqdm(range(n_max), postfix='find_beads: Finding peaks in cross-corelation'):
-        max_index = np.unravel_index(np.argmax(cc, axis=None), cc.shape)
-        arrays = []
-        for roi in [X, Y, cc]:
-            roi, center = get_roi(roi, roi_size, max_index)
-            arrays.append(roi)
-        try:
-            fit, pars = fit_peak(*arrays)
-            if pars[3] < 0.01:
-                break
-            coords_list.append(pars)
-        except (RuntimeError, ValueError) as e:
-            pass
-        cc = set_roi(cc, center, 0 * fit)
-
-    coords = pd.DataFrame(coords_list, columns=['X (pix)', 'Y (pix)', 'width (pix)', 'amplitude (a.u.)', 'R2'])
-
-    if show:
-        print(coords.head(30))
-        fig = plt.figure(figsize=(12, 12))
-        # im = cv2.resize(cc_start, (size * reduction, size * reduction))
-        plt.imshow(im, origin='lower', vmax=np.percentile(im, 99.9), cmap='gray')
-        for i, row in coords.iterrows():
-            color = 'green' if row['R2'] > treshold else 'red'
-            # if color == 'green':
-            box = plt.Rectangle((row['X (pix)'] - roi_size / 2, row['Y (pix)'] - roi_size / 2), roi_size, roi_size,
-                                edgecolor=color, facecolor='none')
-            fig.gca().add_artist(box)
-            plt.text(row['X (pix)'] - roi_size / 2, row['Y (pix)'] + roi_size / 1.9, f'{i}: {row["R2"]:.2f}',
-                     color=color)
-        plt.show()
-
-    coords = coords[coords['R2'] > treshold]
-    coords.sort_values('Y (pix)', inplace=True, ascending=False)
-    coords.reset_index(drop=True, inplace=True)
-    return coords
-
-
 def calc_weight(x, x_array, width):
     weight = np.exp(-2 * (x - x_array) ** 2 / width ** 2)
     return weight / np.sum(weight)
@@ -192,10 +123,10 @@ def multiply_along_axis(A, B, axis):
 
 
 class BeadTracking():
-    def __init__(self, filename=None):
+    def __init__(self, filename=None, pix_um=0.225, n=1.33):
+        self.refraction_index = n
+        self.pix_um = pix_um
         self._from_file(filename)
-        self.refraction_index = 1.33
-        self.pix_um = 0.2
         return
 
     def _from_file(self, filename, highpass=5, lowpass=50):
@@ -205,7 +136,7 @@ class BeadTracking():
             roi_size = np.sqrt(np.size(ims) / np.size(z_calib)).astype(np.int8)
             ims = ims.reshape([-1, roi_size, roi_size])
 
-            z_calib = 1000 * z_calib / 1.33  # to um
+            z_calib = 1000 * z_calib / self.refraction_index  # to um
             lut = [np.abs(get_fft(im, lowpass, highpass)) for im in ims]
 
             self._highpass = highpass
@@ -218,12 +149,85 @@ class BeadTracking():
         new_lut = [np.sum(multiply_along_axis(lut, calc_weight(z_calib, z, step), 0), axis=0) for z in z_array]
         return z_array, new_lut
 
-    def get_xyz(self, im, width=0.4, unit='pix'):
+    def find_beads(self, im, roi_size=100, n_max=100, treshold=None, show=False):
+        @nb.njit(parallel=True, fastmath=True, cache=True)
+        def bin2d(a, K):
+            assert a.shape[0] % K == 0
+            assert a.shape[1] % K == 0
+            m_bins = a.shape[0] // K
+            n_bins = a.shape[1] // K
+            res = np.zeros((m_bins, n_bins), dtype=a.dtype)
+            for i in nb.prange(res.shape[0]):
+                for ii in range(i * K, (i + 1) * K):
+                    for j in range(res.shape[1]):
+                        TMP = res[i, j]
+                        for jj in range(j * K, (j + 1) * K):
+                            TMP += a[ii, jj]
+                        res[i, j] = TMP
+            return res
+
+        reduction = 2  # scale down image by factor 2
+        reduced_im = bin2d(im, reduction)
+        reduced_im -= np.median(reduced_im)
+
+        cc = np.ones_like(reduced_im)
+        for period in tqdm([6.5, 8.7, 10.3, 12.7], postfix='Cross correlating with ref images'):
+            ref_im = create_ref_image(period, size=len(reduced_im))
+            cc *= np.abs(np.fft.ifft2(np.fft.fft2(reduced_im) * np.conjugate(np.fft.fft2(ref_im))))
+        cc = np.abs(np.fft.fftshift(cc))
+        cc /= np.percentile(cc, 99.999)
+
+        X, Y = np.meshgrid(reduction * np.arange(len(cc)), reduction * np.arange(len(cc)))
+
+        coords_list = []
+        for _ in tqdm(range(n_max), postfix='find_beads: Finding peaks in cross-corelation'):
+            max_index = np.unravel_index(np.argmax(cc, axis=None), cc.shape)
+            arrays = []
+            for roi in [X, Y, cc]:
+                roi, center = get_roi(roi, roi_size, max_index)
+                arrays.append(roi)
+            try:
+                fit, pars = fit_peak(*arrays)
+                if pars[3] < 0.01:
+                    break
+                coords_list.append(pars)
+            except (RuntimeError, ValueError) as e:
+                pass
+            cc = set_roi(cc, center, 0 * fit)
+
+        coords = pd.DataFrame(coords_list, columns=['X0 (pix)', 'Y0 (pix)', 'width (pix)', 'amplitude (a.u.)', 'R2'])
+
+        if show:
+            print(coords.head(30))
+            fig = plt.figure(figsize=(12, 12))
+            # im = cv2.resize(cc_start, (size * reduction, size * reduction))
+            plt.imshow(im, origin='lower', vmax=np.percentile(im, 99.9), cmap='gray')
+            for i, row in coords.iterrows():
+                color = 'green' if row['R2'] > treshold else 'red'
+                # if color == 'green':
+                box = plt.Rectangle((row['X0 (pix)'] - roi_size / 2, row['Y0 (pix)'] - roi_size / 2), roi_size,
+                                    roi_size,
+                                    edgecolor=color, facecolor='none')
+                fig.gca().add_artist(box)
+                plt.text(row['X0 (pix)'] - roi_size / 2, row['Y0 (pix)'] + roi_size / 1.9, f'{i}: {row["R2"]:.2f}',
+                         color=color)
+            plt.show()
+
+        coords = coords[coords['R2'] > treshold]
+        coords.sort_values('Y0 (pix)', inplace=True, ascending=False)
+        coords.reset_index(drop=True, inplace=True)
+        self.globals = coords
+        return coords
+
+    def get_roi_xyz(self, im, center=[0, 0], width_um=0.4, unit='pix'):
         def calc_extreme(self, x, y):
-            denom = (x[0] - x[1]) * (x[0] - x[2]) * (x[1] - x[2])
-            A = (x[2] * (y[1] - y[0]) + x[1] * (y[0] - y[2]) + x[0] * (y[2] - y[1])) / denom
-            B = (x[2] * x[2] * (y[0] - y[1]) + x[1] * x[1] * (y[2] - y[0]) + x[0] * x[0] * (y[1] - y[2])) / denom
-            return -B / (2 * A)
+            try:
+                denom = (x[0] - x[1]) * (x[0] - x[2]) * (x[1] - x[2])
+                A = (x[2] * (y[1] - y[0]) + x[1] * (y[0] - y[2]) + x[0] * (y[2] - y[1])) / denom
+                B = (x[2] * x[2] * (y[0] - y[1]) + x[1] * x[1] * (y[2] - y[0]) + x[0] * x[0] * (y[1] - y[2])) / denom
+                return -B / (2 * A)
+            except IndexError:
+                return np.NaN
 
         fft_im = get_fft(im, self._lowpass, self._highpass)
 
@@ -234,17 +238,31 @@ class BeadTracking():
         x = calc_extreme(self, points + peak[0], cc[peak[0] - 1:peak[0] + 2, peak[1]])
         y = calc_extreme(self, points + peak[1], cc[peak[0], peak[1] - 1:peak[1] + 2])
 
-        xy = np.asarray(np.shape(im))*(0.5 + 0.5 * (-0.5 + np.asarray([y, x]) / self._lowpass))
+        xy = np.asarray(np.shape(im)) * (0.5 + 0.5 * (-0.5 + np.asarray([y, x]) / self._lowpass))
+        xy += center
         if unit == 'um':
             xy -= np.asarray(np.shape(im)) / 2
             xy /= self.pix_um
 
         msd = np.sum((self.lut - np.abs(fft_im)) ** 2, axis=(1, 2))
-        weight = calc_weight(self.z_calib[np.argmin(msd)], self.z_calib, width)
+        weight = calc_weight(self.z_calib[np.argmin(msd)], self.z_calib, width_um)
         p = np.polyfit(self.z_calib, msd, 2, w=weight)
         z = -p[1] / (2 * p[0])
 
         return *xy, z
+
+    def process_image(self, im, frame=0):
+        try:
+            self.traces
+        except AttributeError:
+            columns = [[f'{i}: X (pix)', f'{i}: Y (pix)', f'{i}: Z (um)'] for i, _ in
+                       enumerate(self.globals['X0 (pix)'])]
+            self.traces = pd.DataFrame(columns=np.append(['frame'], np.reshape(columns, [-1])))
+
+        xyz = [self.get_roi_xyz(*get_roi(im, 100, (int(x), int(y)))) for x, y in
+               zip(self.globals['X0 (pix)'], self.globals['Y0 (pix)'])]
+        self.traces.loc[len(self.traces.index)] = np.append([frame], np.reshape(xyz, [-1]))
+        return self.traces
 
 
 if __name__ == '__main__':
@@ -252,36 +270,34 @@ if __name__ == '__main__':
     # filename = r'data\test.jpg'
     # frame = aquire_images(500)
     # cv2.imwrite(r'c:\tmp\test.jpg', frame)
-    # im = cv2.imread(filename)[:, :, 0].astype(float)
-    # im, _ = get_roi(im, 3500)
-
-    # coords = find_beads(im, 100, 200, 0.5, show=False)
-    # print(coords)
+    im = cv2.imread(filename)[:, :, 0].astype(float)
+    im, _ = get_roi(im, 3500)
 
     tracker = BeadTracking(filename.replace('.jpg', '.tdms'))
-    ims = np.fromfile(filename.replace('.jpg', 'ROI.bin'), np.uint8).reshape([-1, 100, 100])
-    pos = np.asarray([tracker.get_xyz(im) for im in ims]).T
+    tracker.find_beads(im, 100, 200, 0.5, show=False)
+    for frame in tqdm(range(100)):
+        tracker.process_image(im, frame)
+    print(tracker.traces)
 
-    i = 150
-    plt.imshow(ims[i], cmap='gray')
-    radii = np.linspace(5, 33, 5)
-    for r in radii:
-        circle = plt.Circle(pos[:2, i], r, color='r', fill=False)
-        plt.gca().add_patch(circle)
-
-    plt.show()
+    # ims = np.fromfile(filename.replace('.jpg', 'ROI.bin'), np.uint8).reshape([-1, 100, 100])
+    # pos = np.asarray([tracker.get_xyz(im) for im in ims]).T
+    # i = 153
+    # plt.imshow(ims[i], cmap='gray')
+    # radii = np.linspace(5, 33, 5)
+    # for r in radii:
+    #     circle = plt.Circle(pos[:2, i], r, color='r', fill=False)
+    #     plt.gca().add_patch(circle)
+    #
+    # plt.show()
 
     # plt.plot(z)
     # plt.show()
     # tracker.correct_xy(ims[200])
-    xyz = np.asarray([tracker.get_xyz(im, unit='um') for im in ims]).T
-    for trace in xyz:
-        plt.plot(trace)
-    plt.show()
-    breakpoint()
+    # xyz = np.asarray([tracker.get_xyz(im, unit='um') for im in ims]).T
+    # for trace in xyz:
+    #     plt.plot(trace)
+    # plt.show()
 
-    plt.imshow(lut[60], cmap='gray')
-    plt.show()
     #
     # for im in lut:
     #     cv2.imshow('image', im.astype(np.uint8))
