018d7333902654cc09e3c1c9df7ad5f346f7e214
noort <noort@physics.leidenuniv.nl>
2024-11-06 17:00:29 +0100

    new test AV

---
+++ AV2.py
@@ -0,0 +1,88 @@
+import copy
+import queue
+import threading
+from typing import Optional
+from vmbpy import *
+
+from ImageProcessing import FrameConsumer
+
+FRAME_QUEUE_SIZE = 10
+FRAME_HEIGHT = 480
+FRAME_WIDTH = 480
+
+
+def enqueue_frame(q: queue.Queue, frame_nr, frame: Optional[Frame]):
+    try:
+        q.put_nowait((frame_nr, frame))
+
+    except queue.Full:
+        pass
+
+
+def adjust_cam_setting(cam: Camera, feat_name: str, feat_value: int):
+    feat = cam.get_feature_by_name(feat_name)
+    try:
+        feat.set(feat_value)
+
+    except VmbFeatureError:
+        min_, max_ = feat.get_range()
+        inc = feat.get_increment()
+        if feat_value <= min_:
+            val = min_
+        elif feat_value >= max_:
+            val = max_
+        else:
+            val = (((feat_value - min_) // inc) * inc) + min_
+        feat.set(val)
+
+
+class FrameProducer(threading.Thread):
+    def __init__(self, frame_queue: queue.Queue):
+        threading.Thread.__init__(self)
+        self.log = Log.get_instance()
+
+        vmb = VmbSystem.get_instance()
+        with vmb:
+            for cam in vmb.get_all_cameras():
+                self.cam = cam
+        self.frame_queue = frame_queue
+        self.killswitch = threading.Event()
+
+    def __call__(self, cam: Camera, stream: Stream, frame: Frame):
+        if frame.get_status() == FrameStatus.Complete:
+            if not self.frame_queue.full():
+                frame_cpy = copy.deepcopy(frame)
+                enqueue_frame(self.frame_queue, 0, frame_cpy)
+        cam.queue_frame(frame)
+
+
+    def setup_camera(self):
+        adjust_cam_setting(self.cam, "Height", FRAME_HEIGHT)
+        adjust_cam_setting(self.cam, "Width", FRAME_WIDTH)
+
+        # Try to enable automatic exposure time setting
+        # try:
+        #     self.cam.ExposureAuto.set('Once')
+
+        # except (AttributeError, VmbFeatureError):
+        #     self.log.info('Camera {}: Failed to set Feature \'ExposureAuto\'.'.format(
+        #                   self.cam.get_id()))
+
+        self.cam.set_pixel_format(PixelFormat.Mono8)
+
+    def start(self):
+        try:
+            with self.cam:
+                self.setup_camera()
+                try:
+                    self.cam.start_streaming(self)
+                    self.killswitch.wait()
+                finally:
+                    self.cam.stop_streaming()
+        except VmbCameraError:
+            pass
+        finally:
+            enqueue_frame(self.frame_queue, 0, None)
+
+    def stop(self):
+        self.killswitch.set()
--- DummyCamera.py
+++ DummyCamera.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from time import sleep
 from concurrent.futures import ThreadPoolExecutor
+from AV2 import FrameProducer
 
 try:
     from vmbpy import *
@@ -127,7 +128,7 @@ class CameraApplication:
         return frame
 
     def _produce_frames(self):
-        if DUMMY:
+        if False:
             while self.running:
                 if not self.frame_queue.full():
                     frame = create_dummy_frame(settings=self.settings)
@@ -138,16 +139,7 @@ class CameraApplication:
                         pass
                 threading.Event().wait(1 / 60)  # Simulate 30 FPS
         else:
-            self.allied_vision_camera.start()
-            while self.running:
-                if not self.frame_queue.full():
-                    frame = self.allied_vision_camera.get_frame()
-                    self.frame_nr += 1
-                    try:
-                        self.frame_queue.put_nowait((self.frame_nr, frame))
-                    except queue.Full:
-                        pass
-            self.allied_vision_camera.stop()
+            FrameProducer(self.frame_queue).start()
 
     def _consume_frames(self):
         alive = True
@@ -158,11 +150,14 @@ class CameraApplication:
                 # process_frame(frame_nr, frame, self.settings)
                 # display frames
                 if frame_nr % 1 == 0:
-                    cv_frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-                    plot_rois(cv_frame, self.settings)
-                    cv2.imshow(self.get_camera_id(), cv_frame)
-                    cv2.namedWindow(self.get_camera_id())
-                    cv2.setMouseCallback(self.get_camera_id(), self._mouse_callback)
+                    try:
+                        cv_frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
+                        plot_rois(cv_frame, self.settings)
+                        cv2.imshow(self.get_camera_id(), cv_frame)
+                        cv2.namedWindow(self.get_camera_id())
+                        cv2.setMouseCallback(self.get_camera_id(), self._mouse_callback)
+                    except cv2.error:
+                        pass
             if cv2.waitKey(10) == 13 or not self.running:  # 13 is the Enter key
                 cv2.destroyAllWindows()
                 alive = False
