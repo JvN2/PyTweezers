03184cb2707d553e6733c21881565e5a129196f5
noort <noort@physics.leidenuniv.nl>
2024-11-26 14:21:08 +0100

    logging of steppers

--- MT_steppers.py
+++ MT_steppers.py
@@ -3,6 +3,9 @@
 import threading
 import serial
 import time
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 class StepperApplication:
     def __init__(self, port, baudrate=115200):
@@ -10,6 +13,7 @@ class StepperApplication:
         self.baudrate = baudrate
         self.serial_connection = None
         self.running = False
+        self.df = None
 
     def connect(self):
         self.serial_connection = serial.Serial(self.port, self.baudrate)
@@ -29,36 +33,28 @@ class StepperApplication:
             self.send_gcode(gcode)
 
     def read_response(self):
+        positions = []
         while self.running:
             if self.serial_connection and self.serial_connection.in_waiting > 0:
                 response = self.serial_connection.readline().decode('utf-8').strip()
                 if response[:4] == 'log:':
-                    print(f"Log: {response[4:]}")
+                    positions.append(response[4:].split())
+                if 'Stopped logging' in response:
+                    self.running = False
+                    df = pd.DataFrame(positions[1:], columns=positions[0])
+                    df = df.astype(float)
+                    df.set_index(df.columns[0], inplace=True)
+                    self.df = df
 
 
-    def run(self):
+
+    def run(self, gcodes):
         self.running = True
         self.connect()
         threading.Thread(target=self.read_response).start()
         if self.running:
-            # Example G-code commands
-            gcodes = [
-                "G28 X",  # Home all axes
-                "G93 S0.1"
-                "G1 X1 F30",  # Move to position
-                "M400",
-                "G4 S1",
-                "G1 X0 F30",
-                "G4 S2",
-                "G1 X2 F30",  # Move to position
-                "M400",
-                "G4 S1",
-                "G1 X0 F30"
-                "M400"
-                "G93 0"
-            ]
             self.send_gcodes(gcodes)
-            print("Completed sending G-code commands")
+
 
     def stop(self):
         self.running = False
@@ -66,4 +62,28 @@ class StepperApplication:
 
 if __name__ == "__main__":
     stepper_app = StepperApplication(port='COM3')
-    threading.Thread(target=stepper_app.run).start()
+
+        # Example G-code commands
+    gcodes = [
+        # "G28 X",  # Home all axes
+        "G93 S0.1",
+        "G4 S1"]
+    for i in np.linspace(0, 1  , 100):
+        gcodes.append(f"G1 X{i:3.3f} F{0.5*(i+1)**6:3.3f}")
+    gcodes += [
+        "G4 S1",
+        "G1 X0 F20",  # Move to position
+        "G4 S1",
+        "G94"
+    ]
+
+    for g in gcodes:
+        if "G1" in g:
+            print(g)
+
+
+    threading.Thread(target=stepper_app.run, args=(gcodes,)).start()
+    while stepper_app.df is None:
+        time.sleep(0.5)
+    plt.plot(stepper_app.df['X'])
+    plt.show()
